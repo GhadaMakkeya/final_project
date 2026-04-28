15705ec042158375bed50c2149ec06e9c7dd2b9f
Nora <norabekhet@std.mans.edu.eg>
2026-04-28 18:59:37 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: ThemeData(),
-          home: ProductDetailsScreen(),
+          home: HomeScreen(),
         );
       },
     );
